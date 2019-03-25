ac695db302abb82c58c91e7941098135c08ac941
Tamas Balogh <Tamas.Balogh@logmein.com>
2019-03-25 13:12:21 +0100

    Listen android.intent.action.BOOT_COMPLETED broadcast and start the ImmortalService on the event.

---
+++ app/src/main/java/com/tbalogh/immortalservice/StartImmortalServiceOnBootCompletedReceiver.kt
@@ -0,0 +1,23 @@
+package com.tbalogh.immortalservice
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.support.v4.content.ContextCompat
+import android.util.Log
+
+class StartImmortalServiceOnBootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            Log.d("MyBootCompletedReceiver", "boot completed received")
+            startImmortalService(context)
+        }
+    }
+
+    private fun startImmortalService(context: Context) {
+        Intent(context, ImmortalService::class.java).also { intent ->
+            Log.d("MyBootCompletedReceiver", "starting service directly")
+            ContextCompat.startForegroundService(context, intent)
+        }
+    }
+}
