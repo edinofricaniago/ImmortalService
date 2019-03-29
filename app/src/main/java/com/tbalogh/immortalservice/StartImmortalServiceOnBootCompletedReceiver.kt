44a481d2b3256c285c443d972f32c7aacee0b965
Tamas Balogh <Tamas.Balogh@logmein.com>
2019-03-29 15:30:49 +0100

    Fix reboot

--- app/src/main/java/com/tbalogh/immortalservice/StartImmortalServiceOnBootCompletedReceiver.kt
+++ app/src/main/java/com/tbalogh/immortalservice/StartImmortalServiceOnBootCompletedReceiver.kt
@@ -8,15 +8,18 @@ import android.util.Log
 
 class StartImmortalServiceOnBootCompletedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_LOCKED_BOOT_COMPLETED) {
-            Log.d("MyBootCompletedReceiver", "boot completed received")
-            startImmortalService(context)
+        when(intent.action) {
+            Intent.ACTION_BOOT_COMPLETED, Intent.ACTION_LOCKED_BOOT_COMPLETED -> {
+                Log.d("MyBootCompletedReceiver", "boot completed received")
+                startImmortalService(context)
+            }
         }
     }
 
     private fun startImmortalService(context: Context) {
         Intent(context, ImmortalService::class.java).also { intent ->
             Log.d("MyBootCompletedReceiver", "starting service directly")
+            intent.action = ACTION_START_WITH_KEEP_ALIVE
             ContextCompat.startForegroundService(context, intent)
         }
     }
